10f1c6fe9805e4fa92169c13155f60cb836b3394
CHIA HONG CHANG <vic147569az@gmail.com>
2024-04-21 21:31:16 +0800

    feat(component/app-footer): finish app-footer

--- src/components/app-footer/index.jsx
+++ src/components/app-footer/index.jsx
@@ -1,7 +1,36 @@
 import { memo } from 'react'
+import { FooterWrapper } from './style'
+import footerData from '@/assets/data/footer.json'
 
 const AppFooter = memo(() => {
-  return <div>AppFooter</div>
+  return (
+    <FooterWrapper>
+      <div className="wrapper">
+        <div className="service">
+          {footerData.map((item) => {
+            return (
+              <div className="item" key={item.name}>
+                <div className="name">{item.name}</div>
+                <div className="list">
+                  {item.list.map((subitem) => {
+                    return (
+                      <div className="subitem" key={subitem}>
+                        {subitem}
+                      </div>
+                    )
+                  })}
+                </div>
+              </div>
+            )
+          })}
+        </div>
+        <div className="statement">
+          © 2022 Airbnb, Inc. All rights reserved.条款 · 隐私政策 · 网站地图 · 全国旅游投诉渠道
+          12301
+        </div>
+      </div>
+    </FooterWrapper>
+  )
 })
 
 AppFooter.displayName = 'AppFooter'

c24cbfbd21cc98439fd4bdf7ef011ac3b26ef26f
CHIA HONG CHANG <vic147569az@gmail.com>
2024-04-24 11:36:13 +0800

    feat(view/entire/entire-room): finish entire-rooms

--- src/views/entire/c-cpns/entire-filter/style.js
+++ src/views/entire/c-cpns/entire-filter/style.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components'
 
 export const FilterWrapper = styled.div`
-  position: fixed;
+  /* position: fixed; */
   z-index: 99;
   left: 0;
   right: 0;
--- src/views/entire/c-cpns/entire-rooms/index.jsx
+++ src/views/entire/c-cpns/entire-rooms/index.jsx
@@ -1,9 +1,27 @@
 import { memo } from 'react'
 // import PropTypes from 'prop-types'
 import { RoomsWrapper } from './style'
+import { shallowEqual, useSelector } from 'react-redux'
+import RoomItem from '@/components/room-item'
 
 const EntireRooms = memo(function EntireRooms() {
-  return <RoomsWrapper>EntireRooms</RoomsWrapper>
+  const { roomList, totalCount } = useSelector(
+    (state) => ({
+      roomList: state.entire.roomList,
+      totalCount: state.entire.totalCount
+    }),
+    shallowEqual
+  )
+  return (
+    <RoomsWrapper>
+      <h2 className="title">{totalCount} more house</h2>
+      <div className="list">
+        {roomList.map((item) => {
+          return <RoomItem key={item.id} itemData={item} itemWidth="20%" />
+        })}
+      </div>
+    </RoomsWrapper>
+  )
 })
 
 EntireRooms.propTypes = {}

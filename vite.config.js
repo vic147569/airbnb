d5678874d33c1b2cc36d284f330735a85f48ee6c
CHIA HONG CHANG <vic147569az@gmail.com>
2024-04-25 20:00:13 +0800

    fix(index.html): fix index.html

--- vite.config.js
+++ vite.config.js
@@ -9,5 +9,19 @@ export default defineConfig({
     alias: {
       '@': resolve(__dirname, 'src')
     }
+  },
+  hostname: '0.0.0.0',
+  port: 8080,
+  open: true,
+  https: false,
+  ssr: false,
+  base: './',
+  outDir: 'dist',
+  proxy: {
+    '/api': {
+      target: 'http://codercba.com:1888/airbnb',
+      changeOrigin: true,
+      rewrite: (path) => path.replace(/^\/api/, '')
+    }
   }
 })
